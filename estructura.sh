0ee2e453dc62e5e4b7a32e7d778a2ad96ac7d15a
Isaac <91330447+alurue9140@users.noreply.github.com>
2022-01-21 16:57:30 +0100

    Create estructura.sh

---
+++ estructura.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir proyectos
+
+cd proyectos
+
+touch Proyecto1.pdf
+touch Proyecto2.pdf
+
+mkdir pruebas
+
+cd pruebas
+
+touch prueba1.pdf
+touch prueba2.pdf
+
+cd ..
+
+mkdir src
+
+cd src
+
+touch app1.sh
+touch app2.sh
