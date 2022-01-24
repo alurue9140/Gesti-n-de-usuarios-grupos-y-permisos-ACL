eda280e109f8d9b035c83b0415209a03795de5e5
Isaac <91330447+alurue9140@users.noreply.github.com>
2022-01-24 19:50:18 +0100

    Create añadir.sh

---
+++ añadir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+#Para src
+
+setfacl -R -d -m g:gru_sininformacion:rwx /proyectos/src
+setfacl -R -d -m g:gru_desarrollo:rwx /proyectos/src
+setfacl -R -d -m g:gru_explotacion:rx /proyectos/src
+
+#Para pruebas
+
+setfacl -R -d -m g:gru_sininformacion:rwx /proyectos/pruebas
+setfacl -R -d -m g:gru_explotacion:rwx /proyectos/pruebas
+
+#Para proye
+
+setfacl -R -d -m g:gru_sininformacion:rwx /proyectos
