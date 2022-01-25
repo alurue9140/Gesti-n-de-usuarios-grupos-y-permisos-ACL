00b2ece234b7fa58bc0c4f32a2f3d7d99b23bfe7
Isaac <91330447+alurue9140@users.noreply.github.com>
2022-01-25 11:16:49 +0100

    Update añadir.sh

--- añadir.sh
+++ añadir.sh
@@ -19,11 +19,9 @@ setfacl -R -d -m g:gru_explotacion:rwx /proyectos/pruebas
 setfacl -R -m g:gru_sininformacion:rwx /proyectos/pruebas
 setfacl -R -m g:gru_explotacion:rwx /proyectos/pruebas
 
-
-
 #Para proye
 
 setfacl -R -d -m g:gru_sininformacion:rwx /proyectos
 setfacl -R -m g:gru_sininformacion:rwx /proyectos
-setfacl -m g:gru_desarrollo:x /proyectos
-setfacl -m g:gru_explotacion:x /proyecto
+setfacl -R -m g:gru_desarrollo:x /proyectos
+setfacl -R -m g:gru_explotacion:x /proyectos
